f1e83dc2c7c9cf1a7950f07a5f02145d67a5efa5
Himanshu Dogra <himanshu_dogra@mcafee.com>
2019-11-16 03:02:05 +0530

    beginning GOlang

---
+++ beginningCode/main1.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"stringutil"
+	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
